906f1562be224371f5d2cb89cceca5323e226219
Thibaut Poullain <thibaut.poullain@ydrazil.fr>
2021-08-30 13:46:00 +0200

    v2

--- heroku.js
+++ heroku.js
@@ -1,18 +1,6 @@
+const express = require("express");
+const app = express();
 const fetch = require('node-fetch')
-module.exports = async function(req, res) {
-  if (!req.query.discord || req.body.action !== 'create') {
-    return res.end()
-  }
-  const body = {
-    embeds: [{
-      title: `[${req.body.data.app.name}] ${req.body.data.description}`,
-      description: req.body.data.slug.commit_description,
-      color: 7419530
-    }]
-  }
-  await postTo(req.query.discord, body)
-  res.end()
-}
 
 async function postTo(url, body) {
   return fetch(url, {
@@ -21,3 +9,24 @@ async function postTo(url, body) {
     headers: {'Content-Type': 'application/json'}
   })
 }
+
+app.post("/api/heroku", async (req, res) => {
+  if (!req.query.discord || req.body.action !== 'create') {
+    return res.end()
+  }
+
+  const body = {
+    embeds: [{
+      title: `[${req.body.data.app.name}] ${req.body.data.description}`,
+      description: req.body.data.slug.commit_description,
+      color: 7419530
+    }]
+  }
+
+  await postTo(req.query.discord, body)
+  res.end()
+});
+
+app.listen(3000, () => console.log("App is running on port 3000!"));
+
+
